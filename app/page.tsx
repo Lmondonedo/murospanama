dfe260c38e092fed79cff051bdcac5bbd98074c4
Felrichdev <felrichdev@skunkdev.io>
2025-12-08 18:04:28 -0500

    Se actualiza footer

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ export default async function Home() {
                       d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                     />
                   </svg>
-                  <span>+507 1234-5678</span>
+                  <span>{companyDetails.phone}</span>
                 </div>
                 <div className="flex items-center gap-2">
                   <svg
@@ -151,7 +151,7 @@ export default async function Home() {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  <span>info@murospanama.com</span>
+                  <span>{companyDetails.email}</span>
                 </div>
                 <div className="flex items-center gap-2">
                   <svg
@@ -173,7 +173,7 @@ export default async function Home() {
                       d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
-                  <span>Panamá, República de Panamá</span>
+                  <span>{companyDetails.address}</span>
                 </div>
               </div>
             </div>
